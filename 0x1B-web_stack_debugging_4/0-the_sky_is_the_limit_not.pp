4e7d6ac94072b0e3c27440d242c8dd94b98b906b
Henree001 <okaforhenry779@gmail.com>
2023-06-15 16:35:25 +0100

    fixed identation error in line 5 and 11 in task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,12 +2,12 @@
 
 # Increase the ULIMIT of the default file
 exec { 'nginx_fix':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  command  => 'sed -i "s/15/4096/" /etc/default/nginx',
   provider => shell,
 }
 
 # Restart Nginx
 exec { 'nginx-restart':
-  command => 'service nginx restart',
+  command  => 'service nginx restart',
   provider => shell,
 }
